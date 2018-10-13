02fdba106f19b76a2396d487749a4bfae3bfeb53
Daniil Novikov <dannovik30@gmail.com>
2018-10-13 10:28:20 +0300

    Add img on page

--- injected.js
+++ injected.js
@@ -1,4 +1,6 @@
-var replaced = $("body").html().replace(/Collection/g,'replace');
+var replaced = $("body").html().replace(/Collection/g, 'replace');
 $("body").html(replaced);
-var imgURL = chrome.extension.getURL("images/1.jpg");
-document.getElementsByClassName("list-group-image").src = imgURL;
+
+var image = document.createElement("img");
+image.src = chrome.extension.getURL("images/1.jpg");
+document.getElementsByTagName("body")[0].appendChild(image);
